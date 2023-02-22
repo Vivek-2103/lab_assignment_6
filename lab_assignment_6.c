b61740223722510624e1648f18a86c6d1120833b
Vivek-2103 <125611265+Vivek-2103@users.noreply.github.com>
2023-02-22 14:31:50 -0500

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// This function takes a sorted array of numbers, two integers representing indexes into the array, and another integer to search for.
+// It returns the index in the array where the search value is found between the indexes low and high (inclusive), or -1 if the value is not found.
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	// If the low index is greater than the high index, then the search value is not present in the array between the given indexes.
+	if (low > high) {
+		return -1;
+	}
+
+	// Calculate the middle index of the array between the given indexes.
+	int mid = (low + high) / 2;
+
+	// If the middle element is equal to the search value, then we have found the value and can return its index.
+	if (numbers[mid] == value) {
+		return mid;
+	}
+	// If the middle element is greater than the search value, then the value must be in the left half of the array between the given indexes.
+	else if (numbers[mid] > value) {
+		return search(numbers, low, mid - 1, value);
+	}
+	// Otherwise, the middle element is less than the search value, so the value must be in the right half of the array between the given indexes.
+	else {
+		return search(numbers, mid + 1, high, value);
+	}
 }
 
 void printArray(int numbers[], int sz)
